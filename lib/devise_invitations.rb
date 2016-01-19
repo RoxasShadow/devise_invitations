cc433d1728371ac0d57c3aa34e49de47856919c3
Giovanni Capuano <webmaster@giovannicapuano.net>
2016-01-19 23:02:12 +0100

    devise_invitations: alignment faggottries

--- lib/devise_invitations.rb
+++ lib/devise_invitations.rb
@@ -8,7 +8,7 @@ end
 require 'devise_invitations/engine'
 
 module DeviseInvitations
-  autoload :Mailer, 'devise_invitations/mailer'
+  autoload :Mailer,                'devise_invitations/mailer'
   autoload :InvitationsController, 'devise_invitations/controller'
-  autoload :Invitation, 'devise_invitations/model'
+  autoload :Invitation,            'devise_invitations/model'
 end
